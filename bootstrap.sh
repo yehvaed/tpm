1f413fdeaedd6df3b11f010e3e8b6a12323482e0
yehvaed <jaroslaw.walach@gmail.com>
2023-10-27 14:56:08 +0200

    refactor: command-aliases
    move some part to helper, so I will not need to write run-shell
    everytime

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,12 +11,12 @@ plugins_script_path="${CURRENT_DIR}/scripts/plugins.sh"
 counter=1
 
 new-command() {
-  tmux set -s command-alias[$((1000 + ${counter}))] "$1"="$2"
+  tmux set -s command-alias[$((1000 + ${counter}))] "$1"="run-shell '$2'"
   counter=$(( counter + 1))
 }
 
-new-command "@install" "run-shell '${plugins_script_path}'"
-new-command "@update" "run-shell '${plugins_script_path} update'"
-new-command "@clean" "run-shell '${plugins_script_path} clean'"
+new-command "@install" "${plugins_script_path}"
+new-command "@update" "${plugins_script_path} update"
+new-command "@clean" "${plugins_script_path} clean"
 
-source ${plugins_script_path}
+source ${plugins_script_path} && plugins
